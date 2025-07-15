4eeef42e2f8e1538d91132a388c211425b809ddc
Gabriel Mori <gabrielgimenezmori@gmail.com>
2025-07-15 17:55:53 -0300

    Exercícios 8 até 13 - 16/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -42,23 +42,101 @@ function ex6_comparar() {
 
     resultado = "Os números são iguais"
     if (num1 > num2) {
-        resultado = "Número maior: Número 1 (" + num1 + ")";
+        resultado = num1.toString() + " (Número 1) é o maior";
     } else if (num2 > num1) {
-        resultado = "Número maior: Número 2 (" + num2 + ")";
+        resultado = num2.toString() + " (Número 2) é o maior";
     }
 
-    document.getElementById("ex6-resultado").textContent = resultado;
+    document.getElementById("ex6-resultado").textContent = "Resultado: " + resultado;
 }
 
 function ex7_gerar() {
     let num = Number(document.getElementById("ex7-num").value);
     let div = document.getElementById("ex7-resultado");
 
-    div.innerHTML = ""
+    div.innerHTML = "";
 
     for (let i=1; i<=10; i++) {
-        let p = document.createElement("p")
-        p.textContent = num + " x " + i + " = " + (num*i)
-        div.appendChild(p)
+        let p = document.createElement("p");
+        p.textContent = num + " x " + i + " = " + (num*i);
+        div.appendChild(p);
+    }
+}
+
+function ex8_contar() {
+    let div = document.getElementById("ex8-resultado");
+
+    div.innerHTML = "";
+
+    for (let i=1; i<=10; i++) {
+        let p = document.createElement("p");
+        p.textContent = i;
+        div.appendChild(p);
+    }
+}
+
+function ex9_calcular() {
+    let num1 = Number(document.getElementById("ex9-num1").value);
+    let num2 = Number(document.getElementById("ex9-num2").value);
+    let num3 = Number(document.getElementById("ex9-num3").value);
+
+    let media = ((num1 + num2 + num3) / 3).toFixed(2);
+
+    let resultado = "Aprovado";
+    if (media < 6) {
+        resultado = "Reprovado";
+    }
+
+    document.getElementById("ex9-resultado").textContent = "Resultado: " + resultado + " (Média: " + media + ")";
+}
+
+function ex10_converter() {
+    let num = Number(document.getElementById("ex10-num").value);
+
+    document.getElementById("ex10-resultado").textContent = "Temperatura em °F: " + ((num * 9/5) + 32);
+}
+
+function ex11_verificar() {
+    let num = String(document.getElementById("ex11-num").value);
+
+    let diasDaSemana = {
+        "1": "Domingo",
+        "2": "Segunda-feira",
+        "3": "Terça-feira",
+        "4": "Quarta-feira",
+        "5": "Quinta-feira",
+        "6": "Sexta-feira",
+        "7": "Sábado",
+    }
+
+    let diaDaSemana = diasDaSemana[num];
+    if (diaDaSemana === undefined) {
+        diaDaSemana = "N/A"
+    }
+
+    document.getElementById("ex11-resultado").textContent = "Dia da semana: " + diaDaSemana;
+}
+
+function ex12_verificar() {
+    let num = String(document.getElementById("ex12-num").value);
+
+    resultado = "Senha incorreta"
+    if (num == "1234") {
+        resultado = "Acesso permitido"
+    }
+
+    document.getElementById("ex12-resultado").textContent = resultado;
+}
+
+function ex13_contar() {
+    let div = document.getElementById("ex13-resultado");
+
+    div.innerHTML = "";
+
+    for (let i=10; i>=1; i--) {
+        console.log(i)
+        let p = document.createElement("p");
+        p.textContent = i;
+        div.appendChild(p);
     }
 }
